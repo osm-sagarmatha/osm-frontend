774027bb5b2635b33a580e71b6a6ae348de0e3bb
Ritik <shrestharitik@gmail.com>
2022-06-25 14:37:25 +0545

    final commit 2

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 import { AuthProvider } from "./contexts/auth";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import manifest from "./public/manifest.json";
-
 export default defineConfig({
   plugins: [
     react(),
@@ -10,6 +9,7 @@ export default defineConfig({
       workbox: {
         sourcemap: true,
       },
+      manifest,
     }),
   ],
 });
